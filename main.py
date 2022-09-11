de37f0eea3622784cb9184b87da95c168bdc8cc0
imahdimir <imahdimir@gmail.com>
2022-09-11 15:18:51 +0430

    fv

--- main.py
+++ main.py
@@ -1,119 +1,162 @@
-##
-
 """
 
     """
 
-##
-
-
+import json
 import pandas as pd
 
 from githubdata import GithubData
-from mirutil import funcs as mf
+from mirutil.df_utils import save_as_prq_wo_index as sprq
 
 
-repo_url = 'https://github.com/imahdimir/raw-d-listed-firms-in-TSE'
-tic2btic_repo_url = 'https://github.com/imahdimir/d-Ticker-2-BaseTicker-map'
-btics_repo_url = 'https://github.com/imahdimir/d-uniq-BaseTickers'
+class GDUrl :
+    with open('gdu.json' , 'r') as fi :
+        gj = json.load(fi)
 
-namad = 'نماد'
-btick = 'BaseTicker'
-naam = 'نام - ا'
-tick = 'Ticker'
-cname = 'CompanyName'
+    cur = gj['cur']
+    src = gj['src']
+    src0 = gj['src0']
+    src1 = gj['src1']
+    trg = gj['trg']
+
+gu = GDUrl()
+
+class ColName :
+    namad = 'نماد'
+    btick = 'BaseTicker'
+    naam = 'نام - ا'
+    tick = 'Ticker'
+    cname = 'CompanyName'
+    ftic = 'FirmTicker'
+
+c = ColName()
 
 def main() :
+    pass
 
-  pass
+    ##
 
-  ##
-  repo = GithubData(repo_url)
-  repo.clone_overwrite_last_version()
-  ##
-  dfpn = repo.local_path / 'بورس اوراق بهادار تهران - لیست شرکت ها.xlsx'
-  df = pd.read_excel(dfpn)
-  ##
-  df[namad] = df[namad].apply(mf.norm_fa_str)
-  ##
-  msk = df[namad].ne(namad)
-  df = df[msk]
-  ##
-  ptr = 'ح' + '\s?\.\s.+'
-  msk = df[naam].str.fullmatch(ptr)
-  df = df[~ msk]
-  ##
-  ptr = 'ح' + '\..+'
-  msk = df[naam].str.fullmatch(ptr)
-  df = df[~ msk]
-  ##
-  ptr = 'ح' + '\s.+'
-  msk = df[naam].str.fullmatch(ptr)
-  df = df[~ msk]
-  ##
-  msk = df[namad].eq('سابیک1')
-  assert len(msk[msk]) <= 1
-  df.loc[msk , namad] = 'سآبیک1'
-  ##
+    gds0 = GithubData(gu.src0)
+    gds0.overwriting_clone()
+    ##
+    ds0 = gds0.read_data()
+    ##
 
-  tic2btic_repo = GithubData(tic2btic_repo_url)
-  tic2btic_repo.clone_overwrite_last_version()
-  ##
-  mdfpn = tic2btic_repo.data_fps[0]
-  mdf = pd.read_parquet(mdfpn)
-  mdf = mdf.reset_index()
-  mdf = mdf.set_index(tick)
-  ##
-  df[btick] = df[namad].map(mdf[btick])
-  ##
-  msk = df[btick].isna()
-  df1 = df[msk]
+    gds1 = GithubData(gu.src1)
+    gds1.overwriting_clone()
+    ##
+    ds1 = gds1.read_data()
+    ##
+    ds1 = ds1[[c.btick , c.ftic]]
+    ##
+    ds1 = ds1.drop_duplicates()
+    ##
+    ds1 = ds1.set_index(c.btick)
+    ##
 
-  ##
-  btics_repo = GithubData(btics_repo_url)
-  btics_repo.clone_overwrite_last_version()
-  ##
-  bdfpn = btics_repo.data_fps[0]
-  bdf = pd.read_parquet(bdfpn)
-  bdf = bdf.reset_index()
-  ##
-  df = df[[btick , naam]]
-  ##
-  bdf = bdf.merge(df , how = 'left')
-  ##
-  msk = bdf[cname].notna()
-  msk &= bdf[naam].notna()
-  msk &= bdf[cname].ne(bdf[naam])
+    ds0['f'] = ds0[c.ftic].map(ds1[c.ftic])
+    ##
+    msk = ds0['f'].notna()
 
-  bdf.loc[msk , 'nsn'] = True
-  bdf.loc[~ msk , 'nsn'] = False
+    ds0.loc[msk , c.ftic] = ds0.loc[msk , 'f']
 
-  ##
-  msk = bdf[cname].isna()
-  bdf.loc[msk , cname] = bdf[naam]
+    ##
 
-  ##
-  msk = bdf[cname].isna()
-  df2 = bdf[msk]
+    ds0 = ds0[[c.ftic]]
+    ##
+    ds0 = ds0.drop_duplicates()
+    ##
 
-  ##
-  bdf = bdf[[btick , cname]]
-  bdf = bdf.set_index(btick)
-  ##
-  bdf.to_parquet(bdfpn)
-  ##
-  commit_msg = 'added 13 company names from https://github.com/imahdimir/raw-d-listed-firms-in-TSE'
-  btics_repo.commit_and_push_to_github_data_target(commit_msg)
-  ##
+    gds0.rmdir()
+    gds1.rmdir()
 
-  btics_repo.rmdir()
-  repo.rmdir()
-  tic2btic_repo.rmdir()
+    ##
 
-  ##
+    gds = GithubData(gu.src)
+    gds.overwriting_clone()
+    ##
+    df = gds.read_data()
 
+    ##
+    msk = df[c.namad].ne(c.namad)
+    df = df[msk]
+    ##
+    ptr = 'ح' + '\s?\.\s.+'
+    msk = df[c.naam].str.fullmatch(ptr)
 
-  ##
+    df = df[~ msk]
+    ##
+    ptr = 'ح' + '\..+'
+    msk = df[c.naam].str.fullmatch(ptr)
 
+    df = df[~ msk]
+    ##
+    ptr = 'ح' + '\s.+'
+    msk = df[c.naam].str.fullmatch(ptr)
+
+    df = df[~ msk]
+    ##
+    msk = df[c.namad].eq('سابیک1')
+    assert len(msk[msk]) <= 1
+
+    df.loc[msk , c.namad] = 'سآبیک1'
+
+    ##
+
+    ptr = '\D+1'
+    msk = df[c.namad].str.fullmatch(ptr)
+
+    df1 = df[msk]
+    ##
+    df1[c.namad] = df1[c.namad].str[:-1]
+    ##
+
+    df2 = ds0.merge(df1 , left_on = c.ftic , right_on = c.namad , how = 'left')
+    ##
+    df2 = df2.dropna()
+    df2 = df2.rename(
+            columns = {
+                    c.naam : c.cname
+                    }
+            )
+    df2 = df2[[c.cname , c.ftic]]
+
+    ##
+
+    gdt = GithubData(gu.trg)
+    gdt.overwriting_clone()
+    ##
+    dg = gdt.read_data()
+    ##
+
+    dg = pd.concat([dg , df2] , axis = 0 , ignore_index = True)
+    ##
+    dg = dg.drop_duplicates()
+    ##
+
+    msk = dg.duplicated(subset = c.cname , keep = False)
+    df1 = dg[msk]
+
+    ##
+    fp = gdt.data_fp
+    sprq(dg , fp)
+
+    ##
+    msg = 'added to data by: '
+    msg += gu.cur
+    ##
+
+    gdt.commit_and_push(msg)
+
+    ##
+
+    gds.rmdir()
+    gdt.rmdir()
+
+    ##
+
+##
+if __name__ == '__main__' :
+    main()
 
 ##
